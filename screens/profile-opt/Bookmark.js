5fcf93d83e36b4530a39c7a207d040fbff78856a
HacThienCau <22521641@gm.uit.edu.vn>
2024-12-19 17:41:08 +0700

    update FE

--- screens/profile-opt/Bookmark.js
+++ screens/profile-opt/Bookmark.js
@@ -1,17 +1,54 @@
-import { StyleSheet,View,Text } from "react-native";
+import { StyleSheet,View,Text, FlatList } from "react-native";
+import Thumbnail from "../../component/Thumbnail";
 
 export default function Bookmark(){
+    // Lấy id bài viết trong mục saved từ người dùng x title, image bài viết
+    const list = [
+        {
+            id:1,
+            title:"Bài viết bạn đã lưu (mẫu)",
+            image:"",
+        },
+        {
+            id:2,
+            title:"On iOS you can specify any number of buttons",
+            image:"",
+        },
+        {
+            id:3,
+            title:"On Android at most three buttons can be specified. Android has a concept of a neutral, negative and a positive button",
+            image:"",
+        },
+        {
+            id:4,
+            title:"The cancel event can be handled by providing an onDismiss callback property inside the options parameter.",
+            image:"",
+        },
+    ]
     return(
-        <View style={styles.container}>
-            <Text>Đã lưu</Text>
-        </View>
+    <View style={styles.container}>
+        {list.length==0?
+            <Text style={styles.error}>Không có tin để hiển thị</Text>        
+        :
+      <FlatList
+        data={list}
+        renderItem={({item}) => <Thumbnail id={item.id} title={item.title} image={item.image} nav="Post"/>}
+        keyExtractor={item => item.id}
+      />
+    }
+    </View>
+       
     )
 }
 
 const styles=StyleSheet.create({
     container:{
         flex:1,
-        justifyContent:'center',
-        alignItems: 'center',
+        justifyContent:"flex-start",
+        alignItems: "center",
+    },
+    error:{
+        fontSize:18,
+        marginTop: 50,
     }
 })
--- screens/profile-opt/Notification.js
+++ screens/profile-opt/Notification.js
@@ -1,17 +1,141 @@
-import { StyleSheet,View,Text } from "react-native";
-
-export default function Notification(){
-    return(
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+  Image,
+} from "react-native";
+import { SettingContext } from "../../context/SettingContext";
+import { useContext, useState } from "react";
+export default function Notification() {
+  //lấy từ csdl (tên, hình ảnh, trạng thái) thông báo từ người dùng
+  const [notifList, setList] = useState([
+    {
+      ten: "Đổi mật khẩu thành công",
+      image: "",
+      state: "Chưa đọc",
+    },
+    {
+      ten: "Thông báo 3",
+      image: "",
+      state: "Chưa đọc",
+    },
+    {
+      ten: "Thông báo 2",
+      image: "",
+      state: "Chưa đọc",
+    },
+    {
+      ten: "Thông báo 1",
+      image: "",
+      state: "Đã đọc",
+    },
+    {
+      ten: "Chào mừng bạn đến với AppDocBao của Uyên và Vy :))",
+      image: "",
+      state: "Đã đọc",
+    },
+  ]);
+  const { theme } = useContext(SettingContext);
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "flex-start",
+      alignItems: "center",
+    },
+    scroll: {
+      width: "100%",
+    },
+    error: {
+      fontSize: 18,
+      marginTop: 50,
+    },
+    buttonFrame: {
+      backgroundColor: theme.color,
+      padding: 10,
+      borderRadius: 25,
+      margin: 10,
+    },
+    buttonText: {
+      fontFamily: theme.font.bold,
+      fontSize: 18,
+    },
+    image: {
+      width: "30%",
+      height: 80,
+      backgroundColor: theme.inactive,
+    },
+    notifName: {
+      width: "60%",
+      fontFamily: theme.font.bold,
+      fontSize: 16,
+      flexWrap: "wrap",
+      margin: 10,
+    },
+  });
+  const seen = (ten) => {
+    setList(
+      notifList.map((notif) => {
+        if (notif.ten === ten && notif.state == "Chưa đọc") {
+          return {
+            ...notif,
+            state: "Đã đọc",
+          };
+        } else return notif;
+      })
+    );
+  };
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={styles.buttonFrame}
+        onPress={() => {
+          setList(
+            notifList.map((notif) => {
+              if (notif.state == "Chưa đọc") {
+                return {
+                  ...notif,
+                  state: "Đã đọc",
+                };
+              } else return notif;
+            })
+          );
+        }}
+      >
+        <Text style={styles.buttonText}>Đánh dấu tất cả là đã đọc</Text>
+      </TouchableOpacity>
+      {notifList.length == 0 ? (
         <View style={styles.container}>
-            <Text>Thông báo</Text>
+          <Text style={styles.error}>Bạn không có thông báo nào</Text>
         </View>
-    )
+      ) : (
+        <ScrollView style={styles.scroll}>
+          {notifList.map((notif) => {
+            return (
+              <TouchableOpacity
+                key={notif.ten}
+                onPress={() => {
+                  seen(notif.ten);
+                }}
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  padding: 10,
+                  backgroundColor:
+                    notif.state == "Chưa đọc" ? theme.color : theme.background,
+                  width: "100%",
+                  height: 100,
+                  borderBottomWidth: 1,
+                }}
+              >
+                <Image style={styles.image} src={notif.image} />
+                <Text style={styles.notifName}>{notif.ten}</Text>
+              </TouchableOpacity>
+            );
+          })}
+        </ScrollView>
+      )}
+    </View>
+  );
 }
-
-const styles=StyleSheet.create({
-    container:{
-        flex:1,
-        justifyContent:'center',
-        alignItems: 'center',
-    }
-})
